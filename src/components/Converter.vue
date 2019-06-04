2fa99acd8eebdc6d3647043cb29778f2ab4d7bac
Vladimir Saponko <saponko@gmail.com>
2019-06-04 21:32:26 +0300

    add converter component

---
+++ src/components/Converter.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h2>Input binary number to convert</h2>
+    <form>
+      <label>
+        <input type="text" @keyup="convert" @keydown="restrict" title="Only binary" v-model="number">
+      </label>
+      <h1>Result: {{ result }}</h1>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Converter',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      number: 0,
+      result: 0,
+    }
+  },
+  methods: {
+    convert: function(event) {
+      this.result = parseInt(this.number, 2)
+    },
+    restrict: function (event) {
+      const key = event.key
+      if (key !== '0' && key !== '1' && key !== 'Backspace') {
+        event.preventDefault()
+        return false
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
